792dd2df957ee4d29b97f2d6a3275d02e1fd3272
AperanoGavin <audesandrine6@gmail.com>
2023-02-05 20:24:18 +0100

    add grid , test sound good

--- main.js
+++
@@ -1,9 +0,0 @@
-const cube = document.querySelector('.cube');
-let x = 0;
-let y = 0;
-
-document.addEventListener('mousemove', e => {
-    x = -(window.innerWidth / 2 - e.pageX) / 20;
-    y = (window.innerHeight / 2 - e.pageY) / 20;
-    cube.style.transform = `rotateX(${y}deg) rotateY(${x}deg)`;
-});
