0b43643d746d171272fb23ddd1a8c115bd539b9a
AperanoGavin <audesandrine6@gmail.com>
2023-02-09 18:09:32 +0100

    update realistic map

--- assets/scripts/initMap.js
+++ assets/scripts/initMap.js
@@ -3,12 +3,91 @@ function initMap() {
         lat: 48.88520794099239,
         lng: 2.3053482448301255
     };
+
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 11,
-        center: latlng
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.HYBRID,
+        styles: [{
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#212121"
+                }]
+            },
+            {
+                "elementType": "labels.icon",
+                "stylers": [{
+                    "visibility": "off"
+                }]
+            },
+            {
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#757575"
+                }]
+            },
+            {
+                "elementType": "labels.text.stroke",
+                "stylers": [{
+                    "color": "#212121"
+                }]
+            },
+            {
+                "featureType": "administrative",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#757575"
+                }]
+            },
+            {
+                "featureType": "administrative.country",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#9e9e9e"
+                }]
+            },
+            {
+                "featureType": "administrative.land_parcel",
+                "stylers": [{
+                    "visibility": "off"
+                }]
+            },
+            {
+                "featureType": "administrative.locality",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#bdbdbd"
+                }]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#757575"
+                }]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#181818"
+                }]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#616161"
+                }]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "labels.text.stroke",
+                "stylers": [{
+                    "color": "#1b1b1b"
+                }]
+            }
+        ]
     });
-    var marker = new google.maps.Marker({
-        position: latlng,
-        map: map
-    });
+
 }
