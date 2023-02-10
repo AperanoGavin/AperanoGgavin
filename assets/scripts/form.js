692d32515e5cc3c1d9a3715cb27218fe7c708eae
AperanoGavin <audesandrine6@gmail.com>
2023-02-10 11:37:00 +0100

    build basic form with error when empty

---
+++ assets/scripts/form.js
@@ -0,0 +1,32 @@
+//récuperer la class form-connexion-body et 
+var form = document.querySelector('.form-connexion-body');
+var inputs = form.querySelectorAll('input');
+var submit = form.querySelector('input[type="submit"]');
+var error = document.querySelector('.error');
+var errorText = document.querySelector('.error-text');
+var errorIcon = document.querySelector('.error-icon');
+
+//quand on click sur submit on vérifie si les inputs sont remplis sinon on affiche un message d'erreur
+submit.addEventListener('click', function(e) {
+    e.preventDefault();
+    var empty = false;
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].value === '') {
+            empty = true;
+        }
+    }
+    if (empty) {
+        if ($(".errors").length === 0) {
+            var $newDiv = $("<div></div>");
+            $newDiv.addClass("errors");
+            $(".form-connexion-body").append($newDiv);
+        }
+        $(".errors").empty();
+        $(".errors").append("<i class='fa-regular fa-triangle-exclamation'></i>");
+        $(".errors").append("<span class='error-text'>Veuillez remplir tous les champs</span>");
+    } else {
+        //suprimer $newDiv
+        $(".errors").remove();
+
+    }
+});
