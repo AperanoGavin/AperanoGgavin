a15bb5c2256759bcc72b453ed749187878067d74
AperanoGavin <audesandrine6@gmail.com>
2023-02-22 10:50:58 +0100

    static navbar

---
+++ assets/scripts/sidebar.js
@@ -0,0 +1,5 @@
+//remove les span dans le link avec la class icon qui est dans la div sidebar
+/* var icon = document.querySelectorAll('.icon');
+for (var i = 0; i < icon.length; i++) {
+    icon[i].removeChild(icon[i].querySelector('span'));
+} */
