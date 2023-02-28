ed96e8691930c82eaa305d446c15d1f00c94b8be
AperanoGavin <audesandrine6@gmail.com>
2023-02-28 20:12:02 +0100

    build buger part

--- assets/scripts/sidebar.js
+++ assets/scripts/sidebar.js
@@ -1,5 +1,2 @@
-//remove les span dans le link avec la class icon qui est dans la div sidebar
-/* var icon = document.querySelectorAll('.icon');
-for (var i = 0; i < icon.length; i++) {
-    icon[i].removeChild(icon[i].querySelector('span'));
-} */
+var links = document.querySelectorAll('.side_bar a');
+console.log(links);
