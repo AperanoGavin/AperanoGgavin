2e99b1ce0eb5db108f35aab496c2cf0ec3eb8690
AperanoGavin <audesandrine6@gmail.com>
2023-03-01 21:16:25 +0100

    fist react ok

--- assets/scripts/sidebar.js
+++ assets/scripts/sidebar.js
@@ -1,2 +1,19 @@
-var links = document.querySelectorAll('.side_bar a');
-console.log(links);
+const liens = document.querySelectorAll(".side_bar a");
+liens.forEach(lien => {
+    lien.addEventListener("click", () => {
+        const idSection = lien.getAttribute("id");
+        const Allsections = document.querySelectorAll(".content section");
+        console.log(Allsections);
+        Allsections.forEach(section => {
+            if (section.id === idSection) {
+                //faire remonter le scroll au début de la section
+                section.scrollIntoView();
+
+            } else {
+
+            }
+        });
+
+
+    });
+});
