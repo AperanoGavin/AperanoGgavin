8b53f6e4a76e71bc97f7b1f84703a1421e22bdb3
AperanoGavin <audesandrine6@gmail.com>
2023-03-03 00:02:40 +0100

    carousel ok

--- assets/scripts/carousel.js
+++ assets/scripts/carousel.js
@@ -27,17 +27,17 @@ function currentSlide(n) {
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("carousel-images");
-    //let dots = document.getElementsByClassName("dot");
+    let dots = document.getElementsByClassName("dot");
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    /*     for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" active", "");
-        } */
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
     slides[slideIndex - 1].style.display = "block";
-    //dots[slideIndex - 1].className += " active";
+    dots[slideIndex - 1].className += " active";
 }
 
 var link = document.createElement("a");
