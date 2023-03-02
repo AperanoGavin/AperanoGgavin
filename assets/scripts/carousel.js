aae08893c418f5146905e41a8c942730f330a614
AperanoGavin <audesandrine6@gmail.com>
2023-03-03 00:01:27 +0100

    carousel ok

--- assets/scripts/carousel.js
+++ assets/scripts/carousel.js
@@ -1,4 +1,19 @@
+var count = document.querySelectorAll(".carousel-images").length;
+var div = document.createElement("div");
+div.classList.add("carousel-currentSlider");
+for (let i = 0; i < count; i++) {
+    var span = document.createElement("span");
+    span.classList.add("dot");
+    span.setAttribute("onclick", "currentSlide(" + (i + 1) + ")");
+    div.appendChild(span);
+}
+var maDiv = document.querySelector('.carousel-container');
+maDiv.insertAdjacentElement('afterend', div);
+
+
+
 let slideIndex = 1;
+
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -11,18 +26,18 @@ function currentSlide(n) {
 
 function showSlides(n) {
     let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
+    let slides = document.getElementsByClassName("carousel-images");
+    //let dots = document.getElementsByClassName("dot");
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
+    /*     for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        } */
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    //dots[slideIndex - 1].className += " active";
 }
 
 var link = document.createElement("a");
@@ -37,3 +52,6 @@ linkr.classList.add("next");
 linkr.setAttribute("onclick", "plusSlides(1)");
 linkr.innerHTML = "❯";
 document.querySelector(".carousel-container").appendChild(linkr);
+
+
+var br = document.createElement("br");
