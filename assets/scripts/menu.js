089257468f1764526d09bfc38e74c540d175df8c
AperanoGavin <audesandrine6@gmail.com>
2023-03-03 00:30:30 +0100

    master menu

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -31,3 +31,29 @@ function hideCon() {
     //location.reload();
 
 }
+
+var navbar = document.getElementsByClassName("navbar")[0];
+var userI = document.createElement("i");
+userI.classList.add("fa-solid");
+userI.classList.add("fa-user");
+userI.id = "user";
+userI.setAttribute("onclick", "ShowCon()");
+navbar.appendChild(userI);
+
+var menuI = document.createElement("i");
+menuI.classList.add("fa-solid");
+menuI.classList.add("fa-bars");
+menuI.id = "humberger";
+menuI.setAttribute("onclick", "showMenu()");
+navbar.appendChild(menuI);
+
+
+var navUser = document.getElementsByClassName("nav-user")[0];
+navUser.id = "navUser";
+
+var fasI = document.createElement("i");
+fasI.classList.add("fas");
+fasI.classList.add("fa-times");
+fasI.id = "close";
+fasI.setAttribute("onclick", "hideCon()");
+navUser.insertBefore(fasI, navUser.firstChild);
